13b7bd1d4c729707856579317773ca3fa12aec9e
Sam Jones <samejones2018@gmail.com>
2023-07-08 17:45:06 -0700

    heroku error handleing

--- server/server.js
+++ server/server.js
@@ -66,12 +66,13 @@ const server = new ApolloServer({
   app.use(express.json());
 
   // if we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    });
-    
-  }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
 
   // db.once called when the connection to the database is established
   db.once('open', () => {
